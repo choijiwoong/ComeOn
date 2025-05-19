e8e11b839663762d05422fba7a7e0c6cc1813a78
gogogi313 <gogogi313@naver.com>
2025-05-19 20:45:27 +0900

    feat. Render 배포 준비(requitements.txt추가(라이브러리), app.py포트 설정)

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, render_template, jsonify
 
 app = Flask(__name__)
@@ -19,5 +20,6 @@ def search_api():
     return jsonify({"result": result})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port=int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
 
